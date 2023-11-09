eb35d5e7c76429e3ce86cacba878ea520f9a9026
GEOPREDICT <geos@Masroors-MacBook-Air.local>
2023-11-08 20:25:32 -0800

    Added POST method

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./db');
-const cors = require('cors'); // Import the CORS middleware
+const cors = require('cors'); 
 
 const app = express();
 const port = 8000; // Backend server will run on port 8000
@@ -12,6 +12,20 @@ app.get('/flights', async (req, res) => {
   try {
     const { rows } = await db.query('SELECT * FROM flights');
     res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred.' });
+  }
+});
+
+app.post('/flights', async (req, res) => {
+  try {
+    const { flight_number, name } = req.body; 
+    const query = 'INSERT INTO flights (flight_number, airline_name) VALUES ($1, $2) RETURNING *';
+    const values = [flight_number, name];
+
+    const { rows } = await db.query(query, values);
+    res.json(rows[0]);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred.' });
