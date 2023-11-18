7baade1f24b8bd129206d4174850879981e9d754
GEOPREDICT <geos@Masroors-MacBook-Air.local>
2023-11-17 22:10:35 -0800

    Added tables

---
+++ backend/CoreService.java
@@ -0,0 +1,5 @@
+public class CoreService {
+    public String getGreeting(String name) {
+        return "Hello, " + name + "!";
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+// const { spawn } = require('child_process');
+
 const express = require('express');
 const db = require('./db');
 const cors = require('cors'); 
@@ -7,7 +9,33 @@ const port = 8000; // Backend server will run on port 8000
 
 // Use the CORS middleware to allow requests from http://localhost:3000
 app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(bodyParser.json());
+app.use(express.json());
+
+
+
+//test get
+app.get('/api/greet', (req, res) => {
+  const name = req.query.name || 'Guest';
+
+  // Spawn a Java process
+  const javaProcess = spawn('java', ['/Users/geos/Desktop/ensf/480-Airline/backend/CoreService.java', name]);
+
+  // Capture the output from the Java process
+  let result = '';
+  javaProcess.stdout.on('data', data => {
+      result += data.toString();
+  });
+
+  // Handle the end of the Java process
+  javaProcess.on('close', code => {
+      if (code === 0) {
+          res.send(result);
+      } else {
+          res.status(500).send('Internal Server Error');
+      }
+  });
+});
+
 
 app.get('/flights', async (req, res) => {
   try {
@@ -19,19 +47,56 @@ app.get('/flights', async (req, res) => {
   }
 });
 
-app.post('/flights', async (req, res) => {
-  try {
-    const { flight_number, name } = req.body; 
-    const query = 'INSERT INTO flights (flight_number, airline_name) VALUES ($1, $2) RETURNING *';
-    const values = [flight_number, name];
 
-    const { rows } = await db.query(query, values);
-    res.json(rows[0]);
+app.get('/passenger', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM passengers');
+    res.json(rows);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred.' });
   }
 });
+
+
+
+
+app.use(express.json());
+
+// Endpoint to handle POST requests to insert data into the flights table
+app.post('/flights', (req, res) => {
+  const { id, label, destination, origin, flightNumber, duration } = req.body;
+
+  const query = 'INSERT INTO flights (id, label, destination, origin, flightNumber, duration) VALUES (?, ?, ?, ?, ?, ?)';
+
+  connection.query(query, [id, label, destination, origin, flightNumber, duration], (error, results) => {
+    if (error) {
+      console.error('Error inserting data into flights:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json({ message: 'Flight data inserted successfully' });
+  });
+});
+
+// Endpoint to handle POST requests to insert data into the passengers table
+app.post('/passengers', (req, res) => {
+  const { name, seat, seatType } = req.body;
+
+  const query = 'INSERT INTO passengers (name, seat, seatType) VALUES (?, ?, ?)';
+
+  connection.query(query, [name, seat, seatType], (error, results) => {
+    if (error) {
+      console.error('Error inserting data into passengers:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json({ message: 'Passenger data inserted successfully' });
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
